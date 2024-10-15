7e44a207660d05ab302051ea5abe0510e7a1bd58
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-10-15 18:36:38 +0530

    added some instructions and media queries

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const selector = document.querySelectorAll("selector");
 
 getColorBtn.addEventListener("click", function () {
     colorBox.innerHTML = ""
+    //Fetch get a promise back as response to to decode it we need to use .then()
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorInt.value.slice(1)}&mode=${colorType.value}&count=5`)
+        
+        // Same as fetch .json() returns a promise
       .then((a) => a.json())
+        
+        // Renders the JSON data
         .then((x) => {
+            
             console.log(x)
             console.log(x.colors)
             const a = x.colors
+
             a.forEach(color => {
                 console.log(color.hex.value)
                 const newDiv = document.createElement('div')
@@ -21,7 +28,6 @@ getColorBtn.addEventListener("click", function () {
                 newDiv.appendChild(value)
                 value.textContent = `${color.hex.value}`;
                 value.classList.add("selector")
-                // value.onclick = myFunction()
                 newDiv.style.backgroundColor = `${color.hex.value}`;
             });
         });
